8e33517922dfa42e448d41604170d17c9d6015cc
Eli Mor <elimorr1994@gmail.com>
2021-10-20 03:32:08 +0800

    initial commit to weather App

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './Store/Redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Provider store = {store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
